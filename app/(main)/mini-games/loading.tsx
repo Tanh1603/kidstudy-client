043ad677b0f8a29b1450b76f0d87ae3dc2136e58
Tuananh <tuananh48218@gmail.com>
2025-05-29 20:12:08 +0700

    feat: add LoadingMiniGames component to handle loading state in mini-games

---
+++ app/(main)/mini-games/loading.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/order */
+import Loading from '@/components/loading';
+import React from 'react';
+
+const LoadingMiniGames = () => {
+    return (
+        <Loading/>
+    );
+}
+
+export default LoadingMiniGames;
